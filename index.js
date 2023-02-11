aca078d73e627e6aab63f7b6b49aa75d3bd071a8
md-arif-islam <arifislamarif344@gmail.com>
2023-02-11 09:46:45 +0600

    Static

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.send("This is home page");
