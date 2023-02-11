3c966f29ef3f69e579b2efe9729e56498c196ad1
md-arif-islam <arifislamarif344@gmail.com>
2023-02-11 09:30:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("This is home page");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("This is home page with post request");
+});
+
+app.listen(port, () => {
+  console.log("Listening on post 3000");
+});
